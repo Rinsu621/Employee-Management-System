9dd59cb5934468060c9fe6b94f80f9969aef9901
Rinsu Pradhan <rinsupradhan@gmail.com>
2025-09-15 14:20:59 +0545

    refactor: use Dapper in PatchProjectHandler

--- EmployeeCRUD.Infrastructure/Scripts/PatchProject.sql
+++ EmployeeCRUD.Infrastructure/Scripts/PatchProject.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE PatchProject
-    @Id UNIQUEIDENTIFIER,
+@Id UNIQUEIDENTIFIER,
     @ProjectName VARCHAR(100) = NULL,
     @Description VARCHAR(500) = NULL,
     @ClientName NVARCHAR(100) = NULL,
@@ -9,31 +9,51 @@ CREATE PROCEDURE PatchProject
     @ProjectManagerId UNIQUEIDENTIFIER = NULL,
     @IsArchived BIT = NULL,
     @TeamMembersIds NVARCHAR(MAX) = NULL  
-
 AS
 BEGIN
-SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-UPDATE Projects
-SET
- ProjectName = COALESCE(@ProjectName, ProjectName),
-        Description = COALESCE(@Description, Description),
-        ClientName = COALESCE(@ClientName, ClientName),
-        Status = COALESCE(@Status, Status),
-        Budget = COALESCE(@Budget, Budget),
-        EndDate = COALESCE(@EndDate, EndDate),
-        ProjectManagerId = COALESCE(@ProjectManagerId, ProjectManagerId),
-        IsArchived = COALESCE(@IsArchived, IsArchived)
+
+    UPDATE Projects
+    SET
+        ProjectName= COALESCE(@ProjectName, ProjectName),
+        Description= COALESCE(@Description, Description),
+        ClientName= COALESCE(@ClientName, ClientName),
+        Status= COALESCE(@Status, Status),
+        Budget= COALESCE(@Budget, Budget),
+        EndDate= COALESCE(@EndDate, EndDate),
+        ProjectManagerId= COALESCE(@ProjectManagerId, ProjectManagerId),
+        IsArchived= COALESCE(@IsArchived, IsArchived)
     WHERE Id = @Id;
 
     IF @TeamMembersIds IS NOT NULL
     BEGIN
-    INSERT INTO EmployeeProjects(ProjectsId, TeamMemberId)
-    SELECT @Id, CAST(value AS UNIQUEIDENTIFIER)
-    FROM STRING_SPLIT(@TeamMembersIds,',')
-    WHERE NOT EXISTS(
-    SELECT 1 FROM EmployeeProjects
-    WHERE ProjectsId=@Id AND TeamMemberId=CAST(value AS UNIQUEIDENTIFIER)
-    );
-    END
-    END
+        INSERT INTO EmployeeProjects (ProjectsId, TeamMemberId)
+        SELECT @Id, CAST(value AS UNIQUEIDENTIFIER)
+        FROM STRING_SPLIT(@TeamMembersIds, ',')
+        WHERE NOT EXISTS (
+            SELECT 1 FROM EmployeeProjects
+            WHERE ProjectsId = @Id AND TeamMemberId = CAST(value AS UNIQUEIDENTIFIER)
+        );
+    END;
+
+    SELECT 
+        p.Id,
+        p.ProjectName,
+        p.Description,
+        p.StartDate,
+        p.EndDate,
+        p.Budget,
+        p.Status,
+        p.ClientName,
+        pm.EmpName AS ProjectManagerName,
+        STRING_AGG(e.EmpName, ',') AS TeamMembers
+    FROM Projects p
+    LEFT JOIN Employees pm ON p.ProjectManagerId = pm.Id
+    LEFT JOIN EmployeeProjects ep ON p.Id = ep.ProjectsId
+    LEFT JOIN Employees e ON ep.TeamMemberId = e.Id
+    WHERE p.Id = @Id
+    GROUP BY 
+    p.Id, p.ProjectName, p.Description, p.StartDate, p.EndDate,
+    p.Budget, p.Status, p.ClientName, pm.EmpName;
+END
